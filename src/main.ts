777476d091019a01d547e5ef6a24351fbd6bc27f
corvuscorae <corvuscorae@gmail.com>
2024-11-04 15:47:36 -0800

    draw leaflet map to webpage

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,30 @@
-// todo
-const app: HTMLDivElement = document.querySelector("#app")!;
+import leaflet from "leaflet";
 
-const button = document.createElement("button");
-button.innerHTML = "click here!";
-app.append(button);
+// Style sheets
+import "leaflet/dist/leaflet.css";
+import "./style.css";
 
-const message: string = "you clicked the button!";
-button.addEventListener("click", () => alert(message));
+// debug toolbox
+const debug = {
+  allowZoom: false,
+};
+
+const INIT_LOCATION = leaflet.latLng(36.98949379578401, -122.06277128548504);
+const MAX_ZOOM = 20;
+const MIN_ZOOM = 15;
+const INIT_ZOOM = (MAX_ZOOM + MIN_ZOOM) / 2;
+
+const map = leaflet.map(document.getElementById("map")!, {
+  center: INIT_LOCATION,
+  zoom: INIT_ZOOM,
+  minZoom: MIN_ZOOM,
+  maxZoom: MAX_ZOOM,
+  zoomControl: debug.allowZoom,
+  scrollWheelZoom: debug.allowZoom,
+});
+
+leaflet.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
+  maxZoom: MAX_ZOOM,
+  attribution:
+    '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+}).addTo(map);
