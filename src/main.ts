a33dc5fac08f9c62e0880a4f07985f7b49daa699
corvuscorae <corvuscorae@gmail.com>
2024-11-04 09:17:44 -0800

    L16 dialog -- added button with alert() event

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,9 @@
 // todo
+const app: HTMLDivElement = document.querySelector("#app")!;
+
+const button = document.createElement("button");
+button.innerHTML = "click here!";
+app.append(button);
+
+const message: string = "you clicked the button!";
+button.addEventListener("click", () => alert(message));
