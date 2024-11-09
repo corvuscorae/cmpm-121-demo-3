b9dedba7e9761989329949083ef7d874b06af873
corvuscorae <corvuscorae@gmail.com>
2024-11-08 17:50:47 -0800

    refactor conversion of lat-lng pairs into game cells

---
+++ src/board.ts
@@ -0,0 +1,67 @@
+import leaflet from "leaflet";
+
+export interface Cell {
+  readonly i: number;
+  readonly j: number;
+}
+
+export class Board {
+  readonly tileWidth: number;
+  readonly tileVisibilityRadius: number;
+
+  private readonly knownCells: Map<string, Cell>;
+
+  constructor(tileWidth: number, tileVisibilityRadius: number) {
+    this.tileWidth = tileWidth;
+    this.tileVisibilityRadius = tileVisibilityRadius;
+    this.knownCells = new Map<string, Cell>();
+  }
+
+  private getCanonicalCell(cell: Cell): Cell {
+    const { i, j } = cell;
+    const key = [i, j].toString();
+
+    if (!this.knownCells.has(key)) this.knownCells.set(key, cell);
+
+    return this.knownCells.get(key)!;
+  }
+
+  getCellForPoint(point: leaflet.LatLng): Cell {
+    return this.getCanonicalCell({
+      i: Math.floor(point.lat / this.tileWidth),
+      j: Math.floor(point.lng / this.tileWidth),
+    });
+  }
+
+  getCellBounds(cell: Cell): leaflet.LatLngBounds {
+    const topLeft = {
+      lat: cell.i * this.tileWidth,
+      lng: cell.j * this.tileWidth,
+    };
+    const bottomRight = {
+      lat: (cell.i + 1) * this.tileWidth,
+      lng: (cell.j + 1) * this.tileWidth,
+    };
+    return [topLeft, bottomRight];
+  }
+
+  getCellsNearPoint(point: leaflet.LatLng): Cell[] {
+    const resultCells: Cell[] = [];
+    const originCell = this.getCellForPoint(point);
+
+    // search for cells in tileVisibilityRadius
+    const radius = this.tileVisibilityRadius;
+    for (let i = -radius; i < radius; i++) {
+      for (let j = -radius; j < radius; j++) {
+        resultCells.push(
+          this.getCanonicalCell({
+            i: originCell.i + i,
+            j: originCell.j + j,
+          }),
+        );
+      }
+    }
+
+    return resultCells;
+  }
+}
